1f5ed4fd6bae987d84d9b87c234c1a28d701397f
Shikhar Sharma <ceoshikhar@gmail.com>
2022-08-16 02:21:50 +0530

    feat(math/vec2): sub

--- src/math/vec2.ts
+++ src/math/vec2.ts
@@ -7,4 +7,13 @@ function create<T>(x: T, y: T): Vec2<T> {
     return [x, y];
 }
 
-export { Vec2, create };
+/**
+ * Subtract `what` from `from`.
+ *
+ * sub(from, what) = from - what
+ */
+function sub(from: Vec2<number>, what: Vec2<number>): Vec2<number> {
+    return [from[0] - what[0], from[1] - what[1]];
+}
+
+export { Vec2, create, sub };
