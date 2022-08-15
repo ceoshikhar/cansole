d50731816eaa6e2219e0db0f5dbd2b4020b9e279
Shikhar Sharma <ceoshikhar@gmail.com>
2022-08-15 12:35:04 +0530

    chore: rename `const.ts` to `constants.ts`

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+/**
+ * Global constants.
+ */
+
+/**
+ * Background color for `Cansole`.
+ */
+export const BG_COLOR = "#1B1B1B";
+
+/**
+ * Foreground color for `Cansole`.
+ */
+export const FG_COLOR = "#666666";
