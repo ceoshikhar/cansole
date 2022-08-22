86589ce80dfced5a5ab4d9e55165e3efa9c85277
Shikhar Sharma <ceoshikhar@gmail.com>
2022-08-22 21:01:39 +0530

    feat: `mergeDeep` util

--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,34 @@ function isCanvas(element: HTMLElement): element is HTMLCanvasElement {
     return element instanceof HTMLCanvasElement;
 }
 
+/**
+ * Simple object check.
+ */
+function isObject(item: any): boolean {
+    return item && typeof item === "object" && !Array.isArray(item);
+}
+
+/**
+ * Deep merge two objects.
+ */
+function mergeDeep<T>(target: any, ...sources: any[]): T {
+    if (!sources.length) return target;
+    const source = sources.shift();
+
+    if (isObject(target) && isObject(source)) {
+        for (const key in source) {
+            if (isObject(source[key])) {
+                if (!target[key]) Object.assign(target, { [key]: {} });
+                mergeDeep(target[key], source[key]);
+            } else {
+                Object.assign(target, { [key]: source[key] });
+            }
+        }
+    }
+
+    return mergeDeep(target, ...sources);
+}
+
 // TODO: instead of this we should have "child" <-> "parent" elements to be
 // "drawn" relative to each other?
 function positionButtonRelativeToWindow(cansole: Cansole): void {
@@ -21,4 +49,4 @@ function positionButtonRelativeToWindow(cansole: Cansole): void {
     button.setB(window.box.b - buttonPaddingWithWindow);
 }
 
-export { isCanvas, positionButtonRelativeToWindow };
+export { isCanvas, positionButtonRelativeToWindow, mergeDeep };
